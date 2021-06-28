03bd4e288b53b0b50874fab7f37fc6a76658488c
badkarma10 <86549395+badkarma10@users.noreply.github.com>
2021-06-28 08:28:07 -0700

    Add files via upload

---
+++ rolling dice.py
@@ -0,0 +1,17 @@
+# importing random module
+import random
+
+
+while True:
+    print('Rolling Dice...')
+
+
+    print(f'the value is', random.randint(1, 6))
+
+
+    repeat = input("Roll Dice again? 'y' for yes & 'n' for no: ")
+
+
+    if repeat == 'n':
+
+      "break"
